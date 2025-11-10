63e89515797627482d2e324441e24fbd13b83270
ThePupple <141418165+ThePupple@users.noreply.github.com>
2025-11-10 09:28:01 +0300

    Update DZCICLI.cpp

--- DZCICLI.cpp
+++ DZCICLI.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <string>
+#include <algorithm>
 using namespace std;
 
-// Максимальная и минимальная цифра числа и их произведение
-void max_min_digit_for() {
+// 1. Максимальная и минимальная цифра числа и их произведение
+void max_min_digit() {
     int n;
-    cout << "Enter number: ";
+    cout << "Введите число: ";
     cin >> n;
     
     if (n == 0) {
@@ -13,29 +15,8 @@ void max_min_digit_for() {
     }
     
     int temp = abs(n);
-    int max_digit = temp % 10, min_digit = temp % 10;
-    
-    for (temp /= 10; temp > 0; temp /= 10) {
-        int digit = temp % 10;
-        if (digit > max_digit) max_digit = digit;
-        if (digit < min_digit) min_digit = digit;
-    }
-    cout << "Max: " << max_digit << ", Min: " << min_digit << ", Product: " << max_digit * min_digit << endl;
-}
-
-void max_min_digit_while() {
-    int n;
-    cout << "Enter number: ";
-    cin >> n;
-    
-    if (n == 0) {
-        cout << "Max: 0, Min: 0, Product: 0" << endl;
-        return;
-    }
-    
-    int temp = abs(n);
-    int max_digit = temp % 10, min_digit = temp % 10;
-    temp /= 10;
+    int max_digit = temp % 10;
+    int min_digit = temp % 10;
     
     while (temp > 0) {
         int digit = temp % 10;
@@ -43,89 +24,29 @@ void max_min_digit_while() {
         if (digit < min_digit) min_digit = digit;
         temp /= 10;
     }
-    cout << "Max: " << max_digit << ", Min: " << min_digit << ", Product: " << max_digit * min_digit << endl;
+    
+    cout << "Max: " << max_digit << ", Min: " << min_digit 
+         << ", Product: " << max_digit * min_digit << endl;
 }
 
-void max_min_digit_dowhile() {
-    int n;
-    cout << "Enter number: ";
-    cin >> n;
-    
-    int temp = abs(n);
-    int max_digit = temp % 10, min_digit = temp % 10;
-    temp /= 10;
-    
-    if (temp > 0) {
-        do {
-            int digit = temp % 10;
-            if (digit > max_digit) max_digit = digit;
-            if (digit < min_digit) min_digit = digit;
-            temp /= 10;
-        } while (temp > 0);
-    }
-    cout << "Max: " << max_digit << ", Min: " << min_digit << ", Product: " << max_digit * min_digit << endl;
-}
-
-// Сумма последовательности до 0
-void sum_sequence_for() {
+// 2. Сумма последовательности до 0
+void sum_sequence() {
     int sum = 0, num;
-    cout << "Enter numbers (0 to stop): ";
+    cout << "Введите последовательность чисел (0 для окончания): ";
     
-    for (cin >> num; num != 0; cin >> num) {
-        sum += num;
-    }
-    cout << "Sum: " << sum << endl;
-}
-
-void sum_sequence_while() {
-    int sum = 0, num;
-    cout << "Enter numbers (0 to stop): ";
     cin >> num;
-    
     while (num != 0) {
         sum += num;
         cin >> num;
     }
+    
     cout << "Sum: " << sum << endl;
 }
 
-void sum_sequence_dowhile() {
-    int sum = 0, num;
-    cout << "Enter numbers (0 to stop): ";
-    
-    do {
-        cin >> num;
-        sum += num;
-    } while (num != 0);
-    cout << "Sum: " << sum << endl;
-}
-
-// Удалить заданную цифру из числа
-void remove_digit_for() {
+// 3. Удалить заданную цифру из числа
+void remove_digit() {
     int n, digit;
-    cout << "Enter number and digit to remove: ";
-    cin >> n >> digit;
-    
-    if (n == 0) {
-        cout << "Result: " << (digit == 0 ? 0 : n) << endl;
-        return;
-    }
-    
-    int result = 0, multiplier = 1;
-    for (int temp = abs(n); temp > 0; temp /= 10) {
-        int d = temp % 10;
-        if (d != digit) {
-            result += d * multiplier;
-            multiplier *= 10;
-        }
-    }
-    if (n < 0) result = -result;
-    cout << "Result: " << result << endl;
-}
-
-void remove_digit_while() {
-    int n, digit;
-    cout << "Enter number and digit to remove: ";
+    cout << "Введите число и цифру для удаления: ";
     cin >> n >> digit;
     
     if (n == 0) {
@@ -144,38 +65,15 @@ void remove_digit_while() {
         }
         temp /= 10;
     }
+    
     if (n < 0) result = -result;
     cout << "Result: " << result << endl;
 }
 
-void remove_digit_dowhile() {
-    int n, digit;
-    cout << "Enter number and digit to remove: ";
-    cin >> n >> digit;
-    
-    int result = 0, multiplier = 1;
-    int temp = abs(n);
-    
-    if (temp == 0) {
-        result = (digit == 0 ? 0 : n);
-    } else {
-        do {
-            int d = temp % 10;
-            if (d != digit) {
-                result += d * multiplier;
-                multiplier *= 10;
-            }
-            temp /= 10;
-        } while (temp > 0);
-    }
-    if (n < 0) result = -result;
-    cout << "Result: " << result << endl;
-}
-
-// Делители чисел от N до M
-void find_divisors_for() {
+// 4. Делители чисел от N до M
+void find_divisors() {
     int N, M;
-    cout << "Enter N and M: ";
+    cout << "Введите N и M: ";
     cin >> N >> M;
     
     for (int i = N; i <= M; i++) {
@@ -189,68 +87,16 @@ void find_divisors_for() {
                 found = true;
             }
         }
-        if (!found) cout << "none";
-        cout << endl;
-    }
-}
-
-void find_divisors_while() {
-    int N, M;
-    cout << "Enter N and M: ";
-    cin >> N >> M;
-    int i = N;
-    
-    while (i <= M) {
-        cout << i << ": ";
-        int j = 2;
-        bool found = false;
         
-        while (j < i) {
-            if (i % j == 0) {
-                if (found) cout << ", ";
-                cout << j;
-                found = true;
-            }
-            j++;
-        }
         if (!found) cout << "none";
         cout << endl;
-        i++;
     }
 }
 
-void find_divisors_dowhile() {
-    int N, M;
-    cout << "Enter N and M: ";
-    cin >> N >> M;
-    int i = N;
+// 5. Таблица умножения
+void mult_table() {
+    cout << "Таблица умножения:" << endl;
     
-    if (i <= M) {
-        do {
-            cout << i << ": ";
-            int j = 2;
-            bool found = false;
-            
-            if (j < i) {
-                do {
-                    if (i % j == 0) {
-                        if (found) cout << ", ";
-                        cout << j;
-                        found = true;
-                    }
-                    j++;
-                } while (j < i);
-            }
-            if (!found) cout << "none";
-            cout << endl;
-            i++;
-        } while (i <= M);
-    }
-}
-
-// Таблица умножения
-void mult_table_for() {
-    cout << "Multiplication Table:" << endl;
     for (int i = 1; i <= 10; i++) {
         for (int j = 1; j <= 10; j++) {
             cout << i * j << "\t";
@@ -259,44 +105,157 @@ void mult_table_for() {
     }
 }
 
-void mult_table_while() {
-    cout << "Multiplication Table:" << endl;
-    int i = 1;
+// 6. Сжатие строки (дополнительная функция)
+void compress_string() {
+    string s;
+    cout << "Введите строку для сжатия: ";
+    cin >> s;
     
-    while (i <= 10) {
-        int j = 1;
-        while (j <= 10) {
-            cout << i * j << "\t";
-            j++;
+    string result = "";
+    
+    for (int i = 0; i < s.length(); ) {
+        char current = s[i];
+        int count = 0;
+        
+        while (i < s.length() && s[i] == current) {
+            count++;
+            i++;
         }
-        cout << endl;
-        i++;
+        
+        result += current;
+        result += to_string(count);
     }
+    
+    cout << "Result: " << (result.length() < s.length() ? result : s) << endl;
 }
 
-void mult_table_dowhile() {
-    cout << "Multiplication Table:" << endl;
-    int i = 1;
+// 7. Число в текстовом виде (дополнительная функция)
+void number_to_text() {
+    int n;
+    cout << "Введите число: ";
+    cin >> n;
     
-    do {
-        int j = 1;
-        do {
-            cout << i * j << "\t";
-            j++;
-        } while (j <= 10);
-        cout << endl;
-        i++;
-    } while (i <= 10);
+    if (n == 0) {
+        cout << "ноль" << endl;
+        return;
+    }
+    
+    string ones[] = {"", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять"};
+    string teens[] = {"десять", "одиннадцать", "двенадцать", "тринадцать", "четырнадцать", 
+                     "пятнадцать", "шестнадцать", "семнадцать", "восемнадцать", "девятнадцать"};
+    string tens[] = {"", "", "двадцать", "тридцать", "сорок", "пятьдесят", 
+                    "шестьдесят", "семьдесят", "восемьдесят", "девяносто"};
+    string hundreds[] = {"", "сто", "двести", "триста", "четыреста", "пятьсот", 
+                        "шестьсот", "семьсот", "восемьсот", "девятьсот"};
+    
+    if (n < 0) {
+        cout << "минус ";
+        n = -n;
+    }
+    
+    // Тысячи
+    if (n >= 1000) {
+        int th = n / 1000;
+        
+        if (th >= 100) {
+            cout << hundreds[th / 100] << " ";
+            th %= 100;
+        }
+        if (th >= 20) {
+            cout << tens[th / 10] << " ";
+            th %= 10;
+        } else if (th >= 10) {
+            cout << teens[th - 10] << " ";
+            th = 0;
+        }
+        
+        if (th == 1) cout << "одна тысяча ";
+        else if (th == 2) cout << "две тысячи ";
+        else if (th == 3 || th == 4) cout << ones[th] << " тысячи ";
+        else if (th > 0) cout << ones[th] << " тысяч ";
+        else cout << "тысяч ";
+        
+        n %= 1000;
+    }
+    
+    // Сотни
+    if (n >= 100) {
+        cout << hundreds[n / 100] << " ";
+        n %= 100;
+    }
+    
+    // Десятки и единицы
+    if (n >= 20) {
+        cout << tens[n / 10] << " ";
+        n %= 10;
+    } else if (n >= 10) {
+        cout << teens[n - 10] << " ";
+        n = 0;
+    }
+    
+    if (n > 0) {
+        cout << ones[n];
+    }
+    
+    cout << endl;
+}
+
+// 8. Проверка перестановки строк (дополнительная функция)
+void is_permutation() {
+    string s1, s2;
+    cout << "Введите две строки: ";
+    cin >> s1 >> s2;
+    
+    if (s1.length() != s2.length()) {
+        cout << "false" << endl;
+        return;
+    }
+    
+    sort(s1.begin(), s1.end());
+    sort(s2.begin(), s2.end());
+    
+    cout << (s1 == s2 ? "true" : "false") << endl;
+}
+
+// Главное меню
+void show_menu() {
+    cout << "\n=== ГЛАВНОЕ МЕНЮ ===" << endl;
+    cout << "1. Максимальная и минимальная цифра числа" << endl;
+    cout << "2. Сумма последовательности до 0" << endl;
+    cout << "3. Удалить цифру из числа" << endl;
+    cout << "4. Делители чисел от N до M" << endl;
+    cout << "5. Таблица умножения" << endl;
+    cout << "6. Сжатие строки" << endl;
+    cout << "7. Число в текстовом виде" << endl;
+    cout << "8. Проверка перестановки строк" << endl;
+    cout << "0. Выход" << endl;
+    cout << "Выберите задание: ";
 }
 
 int main() {
-    // Здесь можно вызывать нужные функции для тестирования
-    // Например:
-    // max_min_digit_for();
-    // sum_sequence_while();
-    // remove_digit_for();
-    // find_divisors_while();
-    // mult_table_for();
+    int choice;
+    
+    do {
+        show_menu();
+        cin >> choice;
+        
+        switch (choice) {
+            case 1: max_min_digit(); break;
+            case 2: sum_sequence(); break;
+            case 3: remove_digit(); break;
+            case 4: find_divisors(); break;
+            case 5: mult_table(); break;
+            case 0: cout << "До свидания!" << endl; break;
+            default: cout << "Неверный выбор!" << endl;
+        }
+        
+        if (choice != 0) {
+            cout << "\nНажмите Enter для продолжения...";
+            cin.ignore();
+            cin.get();
+        }
+        
+    } while (choice != 0);
     
     return 0;
 }
