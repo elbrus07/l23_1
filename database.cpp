9d6afaba28c49791308037aabdaad3555c76e4fd
Sarmat <sarmat.toguzov@inbox.ru>
2025-12-09 21:07:07 +0300

    08.12.25

---
+++ database.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <ifstream>
+#include <ofstream>
+#include <string>
+using namespace std;
+
+string getingData(string db_file){
+
+
+}
+
+
+bool addData(object data){
+
+}
+
+template <size_t len>
+string selectData(string (&keys)[len], string filter){
+
+}
+
---
+++ strokes.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+function minimization(str stroke){
+    string letter;
+    for (letter in stroke){
+        return letter;
+    }
+}
+main(){
+    minimization('sarmat');
+    return 0
+}
