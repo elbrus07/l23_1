b1e6134c0721ad1ada038554942db1ded5aef92d
Sarmat <sarmat.toguzov@inbox.ru>
2025-12-10 21:19:51 +0300

    'Modified'

--- database.cpp
+++ database.cpp
@@ -1,22 +1,37 @@
 #include <iostream>
 #include <fstream>
-#include <ifstream>
-#include <ofstream>
+// #include <ifstream>
+// #include <ofstream>
 #include <string>
+#include <vector>
 using namespace std;
 
-string getingData(string db_file){
-
-
+void commandListener(string command){
+    cout<<command;
+    string word = "";
+    string commands[]{};
+    int len = command.length();
+    for (int i = 0; i<len; i++){
+        if (command[i] != ' '){
+            word += command[i];
+        }else{
+            cout<<word<<endl;
+            word = "";
+        }
+    }
 }
 
-
-bool addData(object data){
-
+void runCommand(){
+    string base[]{"ADD", "READ", "FILTERS", "REMOVE", "UPDATE"};
 }
 
-template <size_t len>
-string selectData(string (&keys)[len], string filter){
-
+int main(int argc, char** argv){
+    vector<string> commands;
+    for(int i = 1; i < argc; i++){
+        commands.push_back(argv[i]);
+    }
+    cout<<argv[1]<<endl<<commands[1]<<endl;
+    commandListener(argv[1]);
+    return 0;
 }
 
