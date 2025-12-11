692b5ac6b8088fd293b6c5a1157d00323dd73fc9
Sarmat <sarmat.toguzov@inbox.ru>
2025-12-11 18:33:33 +0300

    11.12.25

--- database.cpp
+++ database.cpp
@@ -6,32 +6,49 @@
 #include <vector>
 using namespace std;
 
-void commandListener(string command){
-    cout<<command;
-    string word = "";
-    string commands[]{};
-    int len = command.length();
-    for (int i = 0; i<len; i++){
-        if (command[i] != ' '){
-            word += command[i];
-        }else{
-            cout<<word<<endl;
-            word = "";
-        }
-    }
+void help(){
+    cout<<"[] - Обязательный параметр"<<"\t"<<"<> - Необязательный параметр"<<endl;
+    cout<<"COMMAND"<<"\t"<<"ARGS"<<"\t"<<"DESCRIPTION"<<endl;
+    cout<<"ADD"<<"\t"<<"[db-file] [VAL [val1], <val2> ...] [TO [col1] <col2> ...]"<<"\t"<<"Добавить значение в столбец"<<endl;
+    cout<<"READ"<<"\t"<<"[db-file] <SECOND-COMMANDS>"<<"\t"<<"Прочитать все строки из db-file"<<endl;
+    cout<<"REMOVE"<<"\t"<<"[db-file] [WHERE [col1] <col2> ...] [EQ [val1], <val2> ...] "<<"\t"<<"Удалить строки где в колонне есть значение val"<<endl;
+    cout<<"SORT"<<"\t"<<"[db-file] [col1] [option] "<<"\t"<<"Сортировать по колонне option-образом"<<endl;
 }
 
-void runCommand(){
-    string base[]{"ADD", "READ", "FILTERS", "REMOVE", "UPDATE"};
+
+void read(vector<string> &com){
+    int len = com.size();
+    vector<string> sec = {"FILTER", "VAL", "TO", "EQ", "WHERE"};
+    if (len == 1){
+        cout<<111<<endl;
+    }
+
+
 }
+
+
+
 
 int main(int argc, char** argv){
     vector<string> commands;
-    for(int i = 1; i < argc; i++){
-        commands.push_back(argv[i]);
+    if (argc == 2 && string(argv[1]) == "help"){
+        help();
+        
     }
-    cout<<argv[1]<<endl<<commands[1]<<endl;
-    commandListener(argv[1]);
+
+    if(argc >= 3 && string(argv[1]) == "READ"){
+        for(int i = 2; i < argc; i++){
+            commands.push_back(argv[i]);
+        }
+        read(commands);
+        cout<<commands.at(0);
+    }
+    
+    // for(int i = 1; i < argc; i++){
+    //     commands.push_back(argv[i]);
+    // }
+    // cout<<argv[1]<<endl<<commands[1]<<endl;
+    // commandListener(argv[1]);
     return 0;
 }
 
