e1d63763247b85fa562d3871ddb0861d0c8ef43d
ThePupple <141418165+ThePupple@users.noreply.github.com>
2026-05-25 08:34:13 +0300

    Update bloknot123.cpp
    переведен на гтк + добавлена подсветка определенных слов

--- bloknot123.cpp
+++ bloknot123.cpp
@@ -1,197 +1,359 @@
-#include <SFML/Graphics.hpp>
+#include <gtk/gtk.h>
 #include <fstream>
 #include <sstream>
 #include <string>
 
-using namespace sf;
 using namespace std;
 
-sf::String loadFromFile(const string& filename)
+GtkWidget* textView;
+
+void highlight_syntax()
 {
-    ifstream file(filename);
+    GtkTextBuffer* buffer =
+        gtk_text_view_get_buffer(GTK_TEXT_VIEW(textView));
+
+    GtkTextIter start, end;
+
+    gtk_text_buffer_get_start_iter(buffer, &start);
+    gtk_text_buffer_get_end_iter(buffer, &end);
+
+    gtk_text_buffer_remove_all_tags(
+        buffer,
+        &start,
+        &end
+    );
+
+    gchar* text =
+        gtk_text_buffer_get_text(buffer, &start, &end, FALSE);
+
+    string content = text;
+
+    GtkTextTagTable* table =
+        gtk_text_buffer_get_tag_table(buffer);
+
+    GtkTextTag* blueTag =
+        gtk_text_tag_table_lookup(table, "blue");
+
+    GtkTextTag* greenTag =
+        gtk_text_tag_table_lookup(table, "green");
+
+    GtkTextTag* redTag =
+        gtk_text_tag_table_lookup(table, "red");
+
+    //подсветка int
+    size_t pos = content.find("int");
+
+    while (pos != string::npos)
+    {
+        GtkTextIter wordStart, wordEnd;
+
+        gtk_text_buffer_get_iter_at_offset(
+            buffer,
+            &wordStart,
+            pos
+        );
+
+        gtk_text_buffer_get_iter_at_offset(
+            buffer,
+            &wordEnd,
+            pos + 3
+        );
+
+        gtk_text_buffer_apply_tag(
+            buffer,
+            blueTag,
+            &wordStart,
+            &wordEnd
+        );
+
+        pos = content.find("int", pos + 1);
+    }
+
+    //подвстветка return
+    pos = content.find("return");
+
+    while (pos != string::npos)
+    {
+        GtkTextIter wordStart, wordEnd;
+
+        gtk_text_buffer_get_iter_at_offset(
+            buffer,
+            &wordStart,
+            pos
+        );
+
+        gtk_text_buffer_get_iter_at_offset(
+            buffer,
+            &wordEnd,
+            pos + 6
+        );
+
+        gtk_text_buffer_apply_tag(
+            buffer,
+            redTag,
+            &wordStart,
+            &wordEnd
+        );
+
+        pos = content.find("return", pos + 1);
+    }
+
+    //подвстветка if
+    pos = content.find("if");
+
+    while (pos != string::npos)
+    {
+        GtkTextIter wordStart, wordEnd;
+
+        gtk_text_buffer_get_iter_at_offset(
+            buffer,
+            &wordStart,
+            pos
+        );
+
+        gtk_text_buffer_get_iter_at_offset(
+            buffer,
+            &wordEnd,
+            pos + 2
+        );
+
+        gtk_text_buffer_apply_tag(
+            buffer,
+            greenTag,
+            &wordStart,
+            &wordEnd
+        );
+
+        pos = content.find("if", pos + 1);
+    }
+
+    g_free(text);
+}
+
+//сохр.
+void save_file(GtkWidget* widget, gpointer data)
+{
+    GtkTextBuffer* buffer =
+        gtk_text_view_get_buffer(GTK_TEXT_VIEW(textView));
+
+    GtkTextIter start, end;
+
+    gtk_text_buffer_get_start_iter(buffer, &start);
+    gtk_text_buffer_get_end_iter(buffer, &end);
+
+    gchar* text =
+        gtk_text_buffer_get_text(buffer, &start, &end, FALSE);
+
+    ofstream file("note.txt");
+    file << text;
+
+    g_free(text);
+}
+
+//откр.
+void open_file(GtkWidget* widget, gpointer data)
+{
+    ifstream file("note.txt");
 
     if (!file.is_open())
-        return "";
+        return;
 
     stringstream buffer;
     buffer << file.rdbuf();
 
-    string loadedText = buffer.str();
+    GtkTextBuffer* textBuffer =
+        gtk_text_view_get_buffer(GTK_TEXT_VIEW(textView));
 
-    return sf::String::fromUtf8(
-        loadedText.begin(),
-        loadedText.end()
+    gtk_text_buffer_set_text(
+        textBuffer,
+        buffer.str().c_str(),
+        -1
     );
+
+    highlight_syntax();
 }
 
-void saveToFile(const string& filename, const sf::String& text)
+//очистка
+void clear_text(GtkWidget* widget, gpointer data)
 {
-    ofstream file(filename);
+    GtkTextBuffer* buffer =
+        gtk_text_view_get_buffer(GTK_TEXT_VIEW(textView));
 
-    file << text.toAnsiString();
+    gtk_text_buffer_set_text(buffer, "", -1);
 }
 
-int main()
+int main(int argc, char* argv[])
 {
-    RenderWindow window(VideoMode(1000, 700), "Mini Notepad");
+    gtk_init(&argc, &argv);
 
-    //ШРИФТ!!!
-    Font font;
 
-    if (!font.loadFromFile("arial.ttf"))
-        return -1;
+    GtkWidget* window =
+        gtk_window_new(GTK_WINDOW_TOPLEVEL);
+
+    gtk_window_set_title(
+        GTK_WINDOW(window),
+        "Mini Notepad"
+    );
+
+    gtk_window_set_default_size(
+        GTK_WINDOW(window),
+        1000,
+        700
+    );
+
+    g_signal_connect(
+        window,
+        "destroy",
+        G_CALLBACK(gtk_main_quit),
+        NULL
+    );
+
+    //контейнер
+    GtkWidget* vbox =
+        gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+
+    gtk_container_add(
+        GTK_CONTAINER(window),
+        vbox
+    );
+
+    // панелька
+    GtkWidget* hbox =
+        gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
+
+    gtk_box_pack_start(
+        GTK_BOX(vbox),
+        hbox,
+        FALSE,
+        FALSE,
+        5
+    );
+
+    //кнопка сохр
+    GtkWidget* saveButton =
+        gtk_button_new_with_label("Save");
+
+    gtk_box_pack_start(
+        GTK_BOX(hbox),
+        saveButton,
+        FALSE,
+        FALSE,
+        5
+    );
+
+    g_signal_connect(
+        saveButton,
+        "clicked",
+        G_CALLBACK(save_file),
+        NULL
+    );
+
+    //кнопка откр
+    GtkWidget* openButton =
+        gtk_button_new_with_label("Open");
+
+    gtk_box_pack_start(
+        GTK_BOX(hbox),
+        openButton,
+        FALSE,
+        FALSE,
+        5
+    );
+
+    g_signal_connect(
+        openButton,
+        "clicked",
+        G_CALLBACK(open_file),
+        NULL
+    );
+
+    //кнопка очистки
+    GtkWidget* clearButton =
+        gtk_button_new_with_label("Clear");
+
+    gtk_box_pack_start(
+        GTK_BOX(hbox),
+        clearButton,
+        FALSE,
+        FALSE,
+        5
+    );
+
+    g_signal_connect(
+        clearButton,
+        "clicked",
+        G_CALLBACK(clear_text),
+        NULL
+    );
+
+    //текст.поле
+    textView = gtk_text_view_new();
+
+    gtk_text_view_set_wrap_mode(
+        GTK_TEXT_VIEW(textView),
+        GTK_WRAP_WORD
+    );
+
+    //буфер 
+    GtkTextBuffer* buffer =
+        gtk_text_view_get_buffer(GTK_TEXT_VIEW(textView));
+
+    // цвета 
+    gtk_text_buffer_create_tag(
+        buffer,
+        "blue",
+        "foreground",
+        "blue",
+        NULL
+    );
+
+    gtk_text_buffer_create_tag(
+        buffer,
+        "green",
+        "foreground",
+        "green",
+        NULL
+    );
+
+    gtk_text_buffer_create_tag(
+        buffer,
+        "red",
+        "foreground",
+        "red",
+        NULL
+    );
 
     
-    sf::String text;
-
-    Text textDisplay;
-    textDisplay.setFont(font);
-    textDisplay.setCharacterSize(24);
-    textDisplay.setFillColor(Color::Black);
-    textDisplay.setPosition(10, 60);
-
-    //панелька
-    RectangleShape topBar(Vector2f(1000, 50));
-    topBar.setFillColor(Color(220, 220, 220));
-
-    //кнопка сохранения save
-    RectangleShape saveButton(Vector2f(120, 35));
-    saveButton.setPosition(10, 7);
-    saveButton.setFillColor(Color(180, 180, 180));
-
-    Text saveText;
-    saveText.setFont(font);
-    saveText.setString("Save");
-    saveText.setCharacterSize(22);
-    saveText.setFillColor(Color::Black);
-    saveText.setPosition(40, 10);
-
-    //кнопка open
-    RectangleShape openButton(Vector2f(120, 35));
-    openButton.setPosition(150, 7);
-    openButton.setFillColor(Color(180, 180, 180));
-
-    Text openText;
-    openText.setFont(font);
-    openText.setString("Open");
-    openText.setCharacterSize(22);
-    openText.setFillColor(Color::Black);
-    openText.setPosition(180, 10);
-
-    //clear кнопка
-    RectangleShape clearButton(Vector2f(120, 35));
-    clearButton.setPosition(290, 7);
-    clearButton.setFillColor(Color(180, 180, 180));
-
-    Text clearText;
-    clearText.setFont(font);
-    clearText.setString("Clear");
-    clearText.setCharacterSize(22);
-    clearText.setFillColor(Color::Black);
-    clearText.setPosition(320, 10);
-
-    while (window.isOpen())
-    {
-        Event event;
-
-        while (window.pollEvent(event))
+    g_signal_connect(
+        buffer,
+        "changed",
+        G_CALLBACK(+[](GtkTextBuffer*, gpointer)
         {
-            
-            if (event.type == Event::Closed)
-                window.close();
+            highlight_syntax();
+        }),
+        NULL
+    );
 
-            //ввод 
-            if (event.type == Event::TextEntered)
-            {
-                // Backspace
-                if (event.text.unicode == 8)
-                {
-                    if (!text.isEmpty())
-                    {
-                        text.erase(text.getSize() - 1, 1);
-                    }
-                }
+    //скролл
+    GtkWidget* scroll =
+        gtk_scrolled_window_new(NULL, NULL);
 
-                // обычные символы
-                else if (event.text.unicode >= 32)
-                {
-                    text += event.text.unicode;
-                }
-            }
+    gtk_container_add(
+        GTK_CONTAINER(scroll),
+        textView
+    );
 
-            //горячие клавиши
-            if (event.type == Event::KeyPressed)
-            {
+    gtk_box_pack_start(
+        GTK_BOX(vbox),
+        scroll,
+        TRUE,
+        TRUE,
+        5
+    );
 
-                // Enter
-                if (event.key.code == Keyboard::Enter)
-                {
-                    text += L'\n';
-                }
+    gtk_widget_show_all(window);
 
-                // ctrl + S
-                if (event.key.control &&
-                    event.key.code == Keyboard::S)
-                {
-                    saveToFile("note.txt", text);
-                }
-
-                // ctrl + O
-                if (event.key.control &&
-                    event.key.code == Keyboard::O)
-                {
-                    text = loadFromFile("note.txt");
-                }
-            }
-
-            
-            if (event.type == Event::MouseButtonPressed)
-            {
-                Vector2f mousePos =
-                    window.mapPixelToCoords(
-                        Mouse::getPosition(window));
-
-                //сохр
-                if (saveButton.getGlobalBounds().contains(mousePos))
-                {
-                    saveToFile("note.txt", text);
-                }
-
-                //открвтие
-                if (openButton.getGlobalBounds().contains(mousePos))
-                {
-                    text = loadFromFile("note.txt");
-                }
-
-                //очистка
-                if (clearButton.getGlobalBounds().contains(mousePos))
-                {
-                    text.clear();
-                }
-            }
-        }
-
-        
-        textDisplay.setString(text);
-
-        //отрисовка всего
-        window.clear(Color::White);
-
-        window.draw(topBar);
-
-        window.draw(saveButton);
-        window.draw(saveText);
-
-        window.draw(openButton);
-        window.draw(openText);
-
-        window.draw(clearButton);
-        window.draw(clearText);
-
-        window.draw(textDisplay);
-
-        window.display();
-    }
+    gtk_main();
 
     return 0;
 }
