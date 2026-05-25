858fd90599f848bf504fb5f15f23221302e2ecd2
sarmatt3 <sarmat.toguzov@inbox.ru>
2026-05-25 10:12:23 +0300

    title

--- gui/notes1.cpp
+++ gui/notes1.cpp
@@ -1,11 +1,16 @@
 #include <gtkmm.h>
 #include <iostream>
 #include <fstream>
+#include <string>
 
 class TextEditorUI : public Gtk::ApplicationWindow {
+
 public:
+std::string filename = "New TextFile";
     TextEditorUI() {
-        set_title("Text Editor UI (gtkmm 4)");
+    
+    set_title("Notes: " + filename);
+        //set_title("Text Editor UI (gtkmm 4)");
         set_default_size(600, 400);
 
         vbox.set_orientation(Gtk::Orientation::VERTICAL);
@@ -19,6 +24,14 @@ public:
         auto action_save = Gio::SimpleAction::create("save");
         action_save->signal_activate().connect([this](const Glib::VariantBase&) { saveDoc(); });
         add_action(action_save);
+        
+        auto action_font = Gio::SimpleAction::create("font");
+        action_font->signal_activate().connect([this](const Glib::VariantBase&) { });
+        add_action(action_font);
+
+        auto action_height = Gio::SimpleAction::create("height");
+        action_height->signal_activate().connect([this](const Glib::VariantBase&) { });
+        add_action(action_height);
 
         auto action_exit = Gio::SimpleAction::create("exit");
         action_exit->signal_activate().connect([this](const Glib::VariantBase&) { close(); });
@@ -31,6 +44,15 @@ public:
         file_menu->append("Save", "win.save");
         file_menu->append("Exit", "win.exit");
         menu->append_submenu("File", file_menu);
+
+        
+        
+       
+        auto file_view = Gio::Menu::create();
+        file_view->append("Font", "win.font");
+        file_view->append("Height", "win.height");
+        
+        menu->append_submenu("View", file_view);
 
         menu_bar.set_menu_model(menu);
         vbox.append(menu_bar);
@@ -50,6 +72,9 @@ private:
     Gtk::PopoverMenuBar menu_bar;
     Gtk::ScrolledWindow scrolled;
     Gtk::TextView text_view;
+    
+    
+    
 
     void openDoc() {
         auto dialog = Gtk::FileDialog::create();
@@ -70,6 +95,9 @@ private:
         try {
             auto file = dialog->open_finish(result);
             std::ifstream inFile(file->get_path());
+            filename = file->get_path();
+            set_title("Notes: " + filename);
+            
             if (inFile) {
                 std::string content((std::istreambuf_iterator<char>(inFile)), std::istreambuf_iterator<char>());
                 text_view.get_buffer()->set_text(content);
@@ -108,6 +136,7 @@ private:
 }
 
     void saveDoc() {
+    if (filename == "New TextFile"){
         auto dialog = Gtk::FileDialog::create();
         dialog->set_title("Сохранить файл");
         dialog->set_accept_label("Сохранить"); // Текст на кнопке подтверждения
@@ -128,7 +157,19 @@ private:
         dialog->set_filters(filters);
 
     // Показываем диалог сохранения
-        dialog->save(*this, sigc::bind(sigc::mem_fun(*this, &TextEditorUI::on_file_saved), dialog));
+        dialog->save(*this, sigc::bind(sigc::mem_fun(*this, &TextEditorUI::on_file_saved), dialog));}else{
+        auto buffer = text_view.get_buffer();
+        auto start = buffer->begin();
+        auto end = buffer->end();
+        std::string content = buffer->get_text(start, end);
+        std::ofstream outFile(filename);
+        if (outFile.is_open()) {
+            outFile << content;
+            outFile.close();
+            std::cout << "Файл сохранён: " << filename << std::endl;
+        } else {
+            std::cerr << "Ошибка: не удалось открыть файл для записи: " << filename << std::endl;
+        };};
 };};
 
 int main(int argc, char* argv[]) {
